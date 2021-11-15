891fd643e5850c5d704e7c6cb7fda424f1982337
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 17:05:01 -0300

    Created User data model

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PloomesCsharpChallenge.Models
+{
+  class User
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string? FirstName { get; set; }
+
+    [MaxLength(30)]
+    public string? LastName { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string? Username { get; set; }
+
+    [Required]
+    public string? AuthToken { get; set; }
+  }
+}
