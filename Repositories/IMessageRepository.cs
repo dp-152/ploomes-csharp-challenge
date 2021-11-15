23a014dbe7bd9e20893582784ba654b19c026b8b
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 20:29:01 -0300

    Changed nullability for return types that require it

--- Repositories/IMessageRepository.cs
+++ Repositories/IMessageRepository.cs
@@ -5,7 +5,7 @@ namespace PloomesCsharpChallenge.Repositories
   public interface IMessageRepository
   {
     bool SaveChanges();
-    Message GetById(int id);
+    Message? GetById(int id);
     Message Create(Message msgData);
     void Update(Message msgData);
     void Delete(Message msgData);
--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -5,8 +5,8 @@ namespace PloomesCsharpChallenge.Repositories
   public interface IUserRepository
   {
     bool SaveChanges();
-    User Register(User userData);
-    User GetByName(string username);
-    User GetById(int id);
+    User? Register(User userData);
+    User? GetByName(string username);
+    User? GetById(int id);
   }
 }
