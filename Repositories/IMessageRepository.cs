466db79c338f3b5053ea822812ceccb79fafc79e
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 18:53:53 -0300

    Created interface for Message data

---
+++ Repositories/IMessageRepository.cs
@@ -0,0 +1,13 @@
+﻿using PloomesCsharpChallenge.Models;
+
+namespace PloomesCsharpChallenge.Repositories
+{
+  public interface IMessageRepository
+  {
+    bool SaveChanges();
+    Message GetById(int id);
+    Message Create(Message msgData);
+    void Update(Message msgData);
+    void Delete(Message msgData);
+  }
+}
