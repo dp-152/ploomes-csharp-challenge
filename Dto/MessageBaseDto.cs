5e971acaa24a9a0e8962a58047cacc93295305e9
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 17:06:52 -0300

    Created DTOs for Message data

---
+++ Dto/MessageBaseDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PloomesCsharpChallenge.Dto
+{
+  class MessageBaseDto
+  {
+    [Required]
+    public int ChatId { get; set; }
+
+    [Required]
+    public int SenderId { get; set; }
+
+    [Required]
+    public string? MessageBody { get; set; }
+  }
+}
