bb028188fa31c70ce6df271cd42bf0dd212a82ea
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 17:29:04 -0300

    Added ChatMembership data model

---
+++ Models/ChatMembership.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PloomesCsharpChallenge.Models
+{
+  class ChatMembership
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int ChatId { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public bool IsAdmin { get; set; }
+  }
+}
