e3827d3fa908c841db9f3e5f2d9f200d2ae6bfd5
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 18:45:30 -0300

    Created interface for Chat data

---
+++ Repositories/IChatRepository.cs
@@ -0,0 +1,17 @@
+﻿using PloomesCsharpChallenge.Models;
+
+namespace PloomesCsharpChallenge.Repositories
+{
+  public interface IChatRepository
+  {
+    IEnumerable<Message> GetMessages(int chatId);
+    IEnumerable<Chat> GetAll();
+    Chat CreatePrivate(Chat chatData);
+    Chat CreateGroup(Chat chatData);
+    void Delete(int chatId);
+    void AddUser(ChatMembership memberData);
+    void RemoveUser(ChatMembership memberData);
+    void SetAdmin(ChatMembership memberData);
+    bool SaveChanges();
+  }
+}
