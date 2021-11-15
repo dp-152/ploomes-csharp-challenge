cc21d2a6bade9443f289ee7bac4bc99e14997213
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 17:05:33 -0300

    Created DTOs for User data

---
+++ Dto/UserBaseDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PloomesCsharpChallenge.Dto
+{
+  class UserBaseDto
+  {
+    [Required]
+    [MaxLength(30)]
+    public string? FirstName { get; set; }
+
+    [MaxLength(30)]
+    public string? LastName { get; set; }
+
+    [Required]
+    [MaxLength(30)]
+    public string? Username { get; set; }
+  }
+}
