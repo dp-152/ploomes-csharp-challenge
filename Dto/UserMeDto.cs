907c3d81c677e53a700c966e700f128879256882
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 03:13:26 -0300

    Added DTO for user creation, which returns auth token

---
+++ Dto/UserMeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PloomesCsharpChallenge.Dto
+{
+  public class UserMeDto : UserBaseDto
+  {
+    public int Id { get; set; }
+    public string? AuthToken { get; set; }
+  }
+}
--- Profiles/UserProfiles.cs
+++ Profiles/UserProfiles.cs
@@ -11,6 +11,9 @@ namespace PloomesCsharpChallenge.Profiles
       // Read (internal -> client)
       CreateMap<User, UserReadDto>();
 
+      // Me (internal -> client)
+      CreateMap<User, UserMeDto>();
+
       // Create (client -> internal)
       CreateMap<UserCreateDto, User>();
     }
