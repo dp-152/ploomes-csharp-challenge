b2efcd0439d7196c52aa00494221fa64e725ee3c
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 04:34:06 -0300

    Removed redundant field from Message base DTO

--- Dto/MessageBaseDto.cs
+++ Dto/MessageBaseDto.cs
@@ -5,9 +5,6 @@ namespace PloomesCsharpChallenge.Dto
   public class MessageBaseDto
   {
     [Required]
-    public int ChatId { get; set; }
-
-    [Required]
     public string MessageBody { get; set; }
   }
 }
--- Dto/MessageReadDto.cs
+++ Dto/MessageReadDto.cs
@@ -3,6 +3,7 @@
   public class MessageReadDto : MessageBaseDto
   {
     public int Id { get; set; }
+    public int ChatId { get; set; }
     public int SenderId { get; set; }
     public DateTime Created { get; set; }
     public DateTime LastChanged { get; set; }
