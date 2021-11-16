668ca8033eda83f35867d506d2a35cedd411267a
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 14:52:14 -0300

    Created endpoint class for Chat API calls

---
+++ Controllers/ChatEndpointsController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PloomesCsharpChallenge.Repositories;
+
+namespace PloomesCsharpChallenge.Controllers
+{
+  public class ChatEndpointsController : ControllerBase
+  {
+    private readonly IUserRepository _userRepository;
+    private readonly IChatRepository _chatRepository;
+    private readonly IMapper _mapper;
+
+    public ChatEndpointsController(
+      IUserRepository userRepository,
+      IChatRepository chatRepository,
+      IMapper mapper)
+    {
+      _userRepository = userRepository;
+      _chatRepository = chatRepository;
+      _mapper = mapper;
+    }
+  }
+}
