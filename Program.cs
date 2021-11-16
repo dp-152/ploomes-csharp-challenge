adf55ad07c737294b1199bd035ed5985f5ff76e0
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 14:46:35 -0300

    Registered JSON contract resolver

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿using PloomesCsharpChallenge.Repositories;
+﻿using Newtonsoft.Json.Serialization;
+
+using PloomesCsharpChallenge.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(s =>
+    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
