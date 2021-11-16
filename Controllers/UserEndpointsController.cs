c698dc2b69140e09954d10f4e62b709a68b7b54e
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 03:14:32 -0300

    Implemented initial user endpoint logic

--- Controllers/UserEndpointsController.cs
+++ Controllers/UserEndpointsController.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using PloomesCsharpChallenge.Dto;
+using PloomesCsharpChallenge.Models;
 using PloomesCsharpChallenge.Repositories;
 
 namespace PloomesCsharpChallenge.Controllers
@@ -24,21 +25,33 @@ namespace PloomesCsharpChallenge.Controllers
     [HttpGet("{id}")]
     public ActionResult<UserReadDto> GetById(int id)
     {
-      throw new NotImplementedException();
+      var user = _repository.GetById(id);
+
+      return user is null ? NotFound() : (ActionResult<UserReadDto>)Ok(_mapper.Map<UserReadDto>(user));
     }
 
     // GET /api/net3/user/{username}
     [HttpGet("username/{username}")]
     public ActionResult<UserReadDto> GetByUsername(string username)
     {
-      throw new NotImplementedException();
+      var user = _repository.GetByName(username);
+      return user is null ? NotFound() : (ActionResult<UserReadDto>)Ok(_mapper.Map<UserReadDto>(user));
     }
 
     // POST /api/net3/user
     [HttpPost]
-    public ActionResult<UserReadDto> Register(UserCreateDto userCreateDto)
+    public ActionResult<UserReadDto> Register(UserCreateDto userData)
     {
-      throw new NotImplementedException();
+      var existingUser = _repository.GetByName(userData.Username);
+
+      if (existingUser is not null)
+      {
+        ModelState.AddModelError("Username", "Username already in use");
+        return ValidationProblem(new ValidationProblemDetails(ModelState));
+      }
+
+      var user = _mapper.Map<User>(userData);
+      return Ok(_mapper.Map<UserMeDto>(_repository.Register(user)));
     }
   }
 }
