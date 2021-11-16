f101762ea3e9ec15897fae42718f676946df3c0e
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 03:49:47 -0300

    Created endpoint class for Message API calls

---
+++ Controllers/MessageEndpointsController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PloomesCsharpChallenge.Repositories;
+
+namespace PloomesCsharpChallenge.Controllers
+{
+  public class MessageEndpointsController : ControllerBase
+  {
+    private readonly IMessageRepository _messageRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IChatRepository _chatRepository;
+    private readonly IMapper _mapper;
+
+    public MessageEndpointsController(
+      IMessageRepository messageRepository,
+      IUserRepository userRepository,
+      IChatRepository chatRepository,
+      IMapper mapper)
+    {
+      _messageRepository = messageRepository;
+      _userRepository = userRepository;
+      _chatRepository = chatRepository;
+      _mapper = mapper;
+    }
+  }
+}
