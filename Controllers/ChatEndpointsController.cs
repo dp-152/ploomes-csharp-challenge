373b0c52e836794f9efe9e86ef25e521cb2db6b4
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 15:09:37 -0300

    Declared endpoints for chat

--- Controllers/ChatEndpointsController.cs
+++ Controllers/ChatEndpointsController.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+using PloomesCsharpChallenge.Dto;
 using PloomesCsharpChallenge.Repositories;
 
 namespace PloomesCsharpChallenge.Controllers
@@ -20,6 +21,76 @@ namespace PloomesCsharpChallenge.Controllers
       _userRepository = userRepository;
       _chatRepository = chatRepository;
       _mapper = mapper;
+    }
+
+    // GET /api/net3/chat
+    [HttpGet("api/net3/chat")]
+    public ActionResult<IEnumerable<ChatReadDto>> GetAllChats()
+    {
+      throw new NotImplementedException();
+    }
+
+    // GET /api/net3/chat/{chatId}
+    [HttpGet("/api/net3/chat/{chatId}")]
+    public ActionResult<ChatReadDto> GetChat(int chatId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // DELETE /api/net3/chat/{chatId}
+    [HttpDelete("/api/net3/chat/{chatId}")]
+    public ActionResult<ChatReadDto> DeleteChat(int chatId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // POST /api/net3/chat/private
+    [HttpPost("/api/net3/chat/private")]
+    public ActionResult<ChatReadDto> CreatePrivateChat([FromBody] ChatCreateDto chatData)
+    {
+      throw new NotImplementedException();
+    }
+
+    // POST /api/net3/chat/group
+    [HttpPost("/api/net3/chat/group")]
+    public ActionResult<ChatReadDto> CreateGroupChat([FromBody] ChatCreateDto chatData)
+    {
+      throw new NotImplementedException();
+    }
+
+    // GET /api/net3/chat/{chatId}/members
+    [HttpGet("/api/net3/chat/{chatId}/members")]
+    public ActionResult<IEnumerable<UserReadDto>> GetChatMembers(int chatId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // GET /api/net3/chat/{chatId}/members/{userId}
+    [HttpGet("/api/net3/chat/{chatId}/members/{userId}")]
+    public ActionResult<ChatReadDto> GetUserMembership(int chatId, int userId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // POST /api/net3/chat/{chatId}/members/{userId}
+    [HttpPost("/api/net3/chat/{chatId}/members/{userId}")]
+    public ActionResult<ChatReadDto> AddUserToChat(int chatId, int userId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // DELETE /api/net3/chat/{chatId}/members/{userId}
+    [HttpDelete("/api/net3/chat/{chatId}/user/{userId}")]
+    public ActionResult<ChatReadDto> RemoveUserFromChat(int chatId, int userId)
+    {
+      throw new NotImplementedException();
+    }
+
+    // POST /api/net3/chat/{chatId}/members/{userId}/admin
+    [HttpPost("/api/net3/chat/{chatId}/members/{userId}/admin")]
+    public ActionResult<ChatReadDto> MakeUserAdmin(int chatId, int userId, [FromBody] ChatMembershipSetAdminDto isAdmin)
+    {
+      throw new NotImplementedException();
     }
   }
 }
