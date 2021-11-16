fc0fb282f657ddc185a7bd0288dc82b56457e11b
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 04:06:44 -0300

    Declared endpoints for message

--- Controllers/MessageEndpointsController.cs
+++ Controllers/MessageEndpointsController.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 
+using PloomesCsharpChallenge.Dto;
 using PloomesCsharpChallenge.Repositories;
 
 namespace PloomesCsharpChallenge.Controllers
@@ -23,6 +24,34 @@ namespace PloomesCsharpChallenge.Controllers
       _userRepository = userRepository;
       _chatRepository = chatRepository;
       _mapper = mapper;
+    }
+
+    // POST /api/net3/message/send/{chatId}
+    [HttpPost("/api/net3/message/send/{chatId}")]
+    public ActionResult<MessageReadDto> Send(int chatId, [FromBody] MessageCreateDto msgData)
+    {
+      throw new NotImplementedException();
+    }
+
+    // GET /api/net3/message/{id}
+    [HttpGet("/api/net3/message/{id}")]
+    public ActionResult<MessageReadDto> GetById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    // PATCH /api/net3/message/{id}
+    [HttpPatch("/api/net3/message/{id}")]
+    public ActionResult Edit(int id, [FromBody] MessageCreateDto patchDocument)
+    {
+      throw new NotImplementedException();
+    }
+
+    // DELETE /api/net3/message/{id}
+    [HttpDelete("/api/net3/message/{id}")]
+    public ActionResult Delete(int id)
+    {
+      throw new NotImplementedException();
     }
   }
 }
