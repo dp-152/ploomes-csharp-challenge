fa2da02fefb568cda15597ca1a3f61eaef206cb0
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 23:18:52 -0300

    Removed unused field

--- Dto/ChatReadDto.cs
+++ Dto/ChatReadDto.cs
@@ -7,6 +7,5 @@ namespace PloomesCsharpChallenge.Dto
     public int Id { get; set; }
     public string? Title { get; set; }
     public string? Type { get; set; }
-    public ICollection<User>? Users { get; set; }
   }
 }
