629ac98f26bf2adcbc7d2b6ccf6a23d8da078918
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 04:32:31 -0300

    Fixed incorrect nullability in User repository interface

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace PloomesCsharpChallenge.Repositories
   public interface IUserRepository
   {
     bool SaveChanges();
-    User? Register(User userData);
+    User Register(User userData);
     User? GetByName(string username);
     User? GetById(int id);
     User? GetByToken(string token);
