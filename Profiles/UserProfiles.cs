197abfd44262bcb801b8ff59a133ed0327558233
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-15 22:42:53 -0300

    Added User data mappings

---
+++ Profiles/UserProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PloomesCsharpChallenge.Dto;
+using PloomesCsharpChallenge.Models;
+
+namespace PloomesCsharpChallenge.Profiles
+{
+  public class UserProfile : Profile
+  {
+    public UserProfile()
+    {
+      // Read (internal -> client)
+      CreateMap<User, UserReadDto>();
+
+      // Create (client -> internal)
+      CreateMap<UserCreateDto, User>();
+    }
+  }
+}
