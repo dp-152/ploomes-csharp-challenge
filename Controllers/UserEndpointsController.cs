bcad7d985a3695352bf73881408acfa342500563
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 02:26:27 -0300

    Created endpoint class for User API calls

---
+++ Controllers/UserEndpointsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PloomesCsharpChallenge.Repositories;
+
+namespace PloomesCsharpChallenge.Controllers
+{
+  [ApiController]
+  [Route("/api/net3/user")]
+  public class UserEndpointsController : ControllerBase
+  {
+    private readonly IUserRepository _repository;
+    private readonly IMapper _mapper;
+
+    public UserEndpointsController(IUserRepository repository, IMapper mapper)
+    {
+      _repository = repository;
+      _mapper = mapper;
+    }
+  }
+}
