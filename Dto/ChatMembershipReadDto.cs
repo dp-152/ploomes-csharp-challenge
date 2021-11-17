a5af9c146c9aca6da0d1e10a8a28e871833ba354
Mizael Almeida <mzalmeida.a@gmail.com>
2021-11-16 21:24:18 -0300

    Removed ID field from ChatMembershipReadDto to minimize confusion

--- Dto/ChatMembershipReadDto.cs
+++ Dto/ChatMembershipReadDto.cs
@@ -2,7 +2,6 @@
 {
   public class ChatMembershipReadDto
   {
-    public int Id { get; set; }
     public int ChatId { get; set; }
     public int UserId { get; set; }
     public bool IsAdmin { get; set; }
